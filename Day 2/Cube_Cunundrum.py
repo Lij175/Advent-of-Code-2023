8918f51d2ec6b12aa5d2fa6922661a28bca80d6c
Lij175 <74162307+Lij175@users.noreply.github.com>
2023-12-11 14:03:10 -0800

    sdaf

--- Day 2/Cube_Cunundrum.py
+++ Day 2/Cube_Cunundrum.py
@@ -1,12 +1,36 @@
 def main():
-    with open('Day 2\game.txt') as doc:
+    #only 12 red cubes, 13 green cubes, and 14 blue cubes
+    with open('Day 2\short game.txt') as doc:
         doc = doc.readlines()
-        doc[-1] += '\n'
-    
-    for line in doc:
-        game = game(line)
-        print(game)
 
+    bag = {'red': 12, 'green': 13, 'blue': 14}
+    
+    game_ID = 0
+    for line in doc:
+        line = line.strip()
+        game = game_obj(line)
+        for draw in game.get_draws():
+            pass
+
+
+
+class game_obj:
+    def __init__(self, game_info):
+        self.num = int(game_info[5])
+        self.draws = game_info[8:].split('; ')
+        for i in range(len(self.draws)):
+            plays = self.draws[i].split(', ')
+            self.draws[i] = dict()
+            for play in plays:
+                self.draws[i][play[2:]] = int(play[0])
+
+
+
+    def get_num(self):
+        return self.num
+    
+    def get_draws(self):
+        return self.draws
 
 if __name__ == "__main__":
     main()

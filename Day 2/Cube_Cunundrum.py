7902b19068b56b82b2b7ce9ce6a888d6c74db012
Elijah Y <74162307+Lij175@users.noreply.github.com>
2023-12-11 08:45:34 -0800

    qwe

--- Day 2/Cube_Cunundrum.py
+++ Day 2/Cube_Cunundrum.py
@@ -1,5 +1,9 @@
 def main():
-    pass
+    with open('Day 1\calibration document.txt') as doc:
+        doc = doc.readlines()
+        doc[-1] += '\n'
+    
+    print(doc)
 
 
 if __name__ == "__main__":

03a7f05b3af950593ebbe0b5fc79f7e020fedf63
Lij175 <74162307+Lij175@users.noreply.github.com>
2023-12-11 21:57:03 -0800

    part 1 done

--- Day 3 - Gear Ratios/Gear_Ratios.py
+++ Day 3 - Gear Ratios/Gear_Ratios.py
@@ -1,44 +1,57 @@
 def main():
-    with open('Day 3 - Gear Ratios\smol schematic.txt') as doc:
+    with open('Day 3 - Gear Ratios\engine schematic.txt') as doc:
         doc = doc.readlines()
-    schematic = []
+    schematic = [['.' for n in range(len(doc[0]) + 1)]]
     for line in doc:
-        line.strip()
-        list_of_line = []
+        line = line.strip()
+        list_of_line = ['.']
         for symbol in line:
             list_of_line.append(symbol)
+        list_of_line.append('.')
         schematic.append(list_of_line)
+    schematic.append(['.' for n in range(len(doc[0]) + 1)])
     
-def get_surrounding(number, schematic):
     '''
-    numer in format of ((1,2), (1,3), (1,4)). ie. indexs of number
+    for line in schematic:
+        print(''.join(line))
     '''
-    num_rows = len(schematic)
-    num_cols = len(schematic[0])
-    if number[0][0] == 0 and number[0][1] == 0:
-        # top left
-        pass
-    elif number[-1][0] == 0 and number[-1][1] == num_cols - 1:
-        # top right
-        pass
-    elif number[0][0] == num_rows - 1 and number[0][1] == 0:
-        # bottom left
-        pass
-    elif number[-1][0] == num_rows - 1 and number[-1][1] == num_cols - 1:
-        # bottom right
-        pass
-    elif number[0][0] == 0:
-        # top row
-        pass
-    elif number[0][0] == num_rows - 1:
-        # bottom row
-        pass
-    elif number[0][1] == 0:
-        # left col
-        pass
-    elif number[-1][1] == num_cols - 1:
-        # right col
-        pass
+    part1(schematic)
+
+
+def part1(schematic):
+    sum = 0
+    num = 0
+    num_loc = list()
+    for row in range(len(schematic)):
+        for col in range(len(schematic[row])):
+            if schematic[row][col] in '1234567890':
+                num = num * 10 + int(schematic[row][col])
+                num_loc.append([row, col])
+            elif num != 0:
+                if(has_adjacent(num_loc, schematic)):
+                    #print(num_loc)
+                    sum += num
+                num = 0
+                num_loc.clear()
+    
+    print(sum)
+
+    
+    
+def has_adjacent(num_loc, schematic):
+    '''
+    numer in format of [[1,2], [1,3], [1,4]]. ie. indexs of number
+    '''
+    
+    for index in num_loc:
+        row, col = index
+        if schematic[row-1][col-1] not in '1234567890.' or schematic[row][col-1] not in '1234567890.' or schematic[row+1][col-1] not in '1234567890.' or\
+            schematic[row-1][col] not in '1234567890.' or schematic[row][col] not in '1234567890.' or schematic[row+1][col] not in '1234567890.' or\
+            schematic[row-1][col+1] not in '1234567890.' or schematic[row][col+1] not in '1234567890.' or schematic[row+1][col+1] not in '1234567890.':
+            return True
+
+    
+    return False
 
 
 if __name__ == "__main__":
